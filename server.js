16e3edb9e502cd36282eecfafaee98ebfbf27fff
Cesar <rodascesar03@yahoo.com>
2017-01-27 21:02:32 -0600

    Finished simple game api

--- server.js
+++ server.js
@@ -24,12 +24,62 @@ const server = app.listen(process.env.PORT || 8080, function() {
 app.get("/games", function(req, res) {
   VideoGame.find({})
     .then((users) => {
-      console.log("users " ,users);
-      console.log("typeof users ", typeof users);
-      console.log("keys", Object.keys(users));
       res.send(users);
     })
     .catch(() => {
-      res.send("sorry");
+      res.send("Was not able to bring game information.");
     })
+});
+
+app.post("/games", function(req, res) {
+  var game = new VideoGame();
+  console.log(req.body);
+  game.title = req.body.title;
+  game.description = req.body.description;
+  game.img = req.body.img;
+
+  game.save((err) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json({ message: 'Videogame created!'});
+  });
+});
+
+app.get("/games/:_id", function(req, res) {
+  VideoGame.findById(req.params._id, (err, game) => {
+    if(err) {
+      res.send(err);
+    }
+    res.json(game);
+  });
+});
+
+app.put("/games/:_id", function(req, res) {
+  VideoGame.findById(req.params._id, (err, game) => {
+    if(err){
+      res.send(err);
+    }
+    game.title = req.body.title ? req.body.title : game.title;
+    game.description = req.body.description ? req.body.description : game.description;
+    game.img = req.body.img ? req.body.img : game.img;
+
+    game.save((err) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json({ message: 'Game updated!' });
+    });
+  });
+});
+
+app.delete("/games/:_id", function(req, res) {
+  VideoGame.remove({
+    _id: req.params._id
+  }, (err, game) => {
+    if(err) {
+      res.send(err);
+    }
+    res.json({ message: 'successfully deleted' });
+  });
 });
